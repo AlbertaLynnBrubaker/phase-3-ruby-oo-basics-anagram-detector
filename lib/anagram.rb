43386fe6726183676110fde5ffe4ed06a9d74a47
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-08 15:55:48 -0500

    commit tree f3377e786b0ba497e2fa690a00eaff96763964dc

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  def initialize word
+    @word = word
+  end
+  
+  array = %w[]
+  
+  def match array
+    array.filter { |new_word|
+      @word.chars.sort == new_word.chars.sort
+    }
+  end
+  
+end
